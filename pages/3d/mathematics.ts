05fd3bbe4983ed4c0c74867423909c431fa5fe92
tnRaro <deokji53@gmail.com>
2020-07-05 15:59:35 +0900

    improve mathematics

--- pages/3d/mathematics.ts
+++ pages/3d/mathematics.ts
@@ -1,37 +1,52 @@
 export type float = number;
+/**
+ * 0\
+ * 1\
+ * 2\
+ * 3
+ */
+export type floats = float2 | float3 | float4;
 export type float2 = [number, number];
 export type float3 = [number, number, number];
 export type float4 = [number, number, number, number];
-export type matrix2x2 = [
-  number, number,
-  number, number
-];
-export type matrix3x3 = [
-  number, number, number,
-  number, number, number,
-  number, number, number
-];
-export type matrix4x4 = [
-  number, number, number, number,
-  number, number, number, number,
-  number, number, number, number,
-  number, number, number, number,
-];
+/**
+ * 00 10 20 30\
+ * 01 11 21 31\
+ * 02 12 22 32\
+ * 03 13 23 33
+ */
+export type matrix =
+  matrix2x2 | matrix2x3 | matrix2x4 |
+  matrix3x2 | matrix3x3 | matrix3x4 |
+  matrix4x2 | matrix4x3 | matrix4x4;
+/** square matrix */
+export type square =
+  matrix2x2 | matrix3x3 | matrix4x4;
+export type matrix2x2 = [float2, float2];
+export type matrix2x3 = [float2, float2, float2];
+export type matrix2x4 = [float2, float2, float2, float2];
+export type matrix3x2 = [float3, float3];
+export type matrix3x3 = [float3, float3, float3];
+export type matrix3x4 = [float3, float3, float3, float3];
+export type matrix4x2 = [float4, float4];
+export type matrix4x3 = [float4, float4, float4];
+export type matrix4x4 = [float4, float4, float4, float4];
+
 // cast object to mathematics type
 export const float = (v: number): float => v;
 export function float2(x: { x: number, y: number }): float2;
-export function float2(x: float2): float2;
+export function float2(x: floats): float2;
 export function float2(x: any): float2 {
   if (Array.isArray(x))
-    return x as float2;
+    return [x[0], x[1]];
   else
     return [x.x, x.y];
 }
 export function float3(x: { x: number, y: number, z: number }): float3;
-export function float3(x: float3): float3;
+export function float3(x: float3 | float4): float3;
 export function float3(x: any): float3 {
   if (Array.isArray(x))
-    return x as float3;
+    return [x[0], x[1], x[2]];
   else
     return [x.x, x.y, x.z];
 }
@@ -39,114 +54,118 @@ export function float4(x: { x: number, y: number, z: number, w: number }): float
 export function float4(x: float4): float4;
 export function float4(x: any): float4 {
   if (Array.isArray(x))
-    return x as float4;
+    return [x[0], x[1], x[2], x[4]];
   else
     return [x.x, x.y, x.z, x.w];
 }
 // float to matrix
 export const matrix2x2 = (f0: float2, f1: float2): matrix2x2 => [
-  f0[0], f1[0],
-  f0[1], f1[1],
+  [f0[0], f0[1]],
+  [f1[0], f1[1]]
+];
+export const matrix2x3 = (f0: float2, f1: float2, f2: float2): matrix2x3 => [
+  [f0[0], f0[1]],
+  [f1[0], f1[1]],
+  [f2[0], f2[1]],
+];
+export const matrix2x4 = (f0: float2, f1: float2, f2: float2, f3: float2): matrix2x4 => [
+  [f0[0], f0[1]],
+  [f1[0], f1[1]],
+  [f2[0], f2[1]],
+  [f3[0], f3[1]],
+];
+export const matrix3x2 = (f0: float3, f1: float3): matrix3x2 => [
+  [f0[0], f0[1], f0[2]],
+  [f1[0], f1[1], f1[2]]
 ];
 export const matrix3x3 = (f0: float3, f1: float3, f2: float3): matrix3x3 => [
-  f0[0], f1[0], f2[0],
-  f0[1], f1[1], f2[1],
-  f0[2], f1[2], f2[2],
+  [f0[0], f0[1], f0[2]],
+  [f1[0], f1[1], f1[2]],
+  [f2[0], f2[1], f2[2]],
+];
+export const matrix3x4 = (f0: float3, f1: float3, f2: float3, f3: float3): matrix3x4 => [
+  [f0[0], f0[1], f0[2]],
+  [f1[0], f1[1], f1[2]],
+  [f2[0], f2[1], f2[2]],
+  [f3[0], f3[1], f3[2]],
+];
+export const matrix4x2 = (f0: float4, f1: float4): matrix4x2 => [
+  [f0[0], f0[1], f0[2], f0[3]],
+  [f1[0], f1[1], f1[2], f1[3]]
+];
+export const matrix4x3 = (f0: float4, f1: float4, f2: float4): matrix4x3 => [
+  [f0[0], f0[1], f0[2], f0[3]],
+  [f1[0], f1[1], f1[2], f1[3]],
+  [f2[0], f2[1], f2[2], f2[3]],
 ];
 export const matrix4x4 = (f0: float4, f1: float4, f2: float4, f3: float4): matrix4x4 => [
-  f0[0], f1[0], f2[0], f3[0],
-  f0[1], f1[1], f2[1], f3[1],
-  f0[2], f1[2], f2[2], f3[2],
-  f0[3], f1[3], f2[3], f3[3],
+  [f0[0], f0[1], f0[2], f0[3]],
+  [f1[0], f1[1], f1[2], f1[3]],
+  [f2[0], f2[1], f2[2], f2[3]],
+  [f3[0], f3[1], f3[2], f3[3]],
 ];
-export function add(m: float2, n: float2): float2;
-export function add(m: float3, n: float3): float3;
-export function add(m: float4, n: float4): float4;
-export function add(m: matrix2x2, n: matrix2x2): matrix2x2;
-export function add(m: matrix3x3, n: matrix3x3): matrix3x3;
-export function add(m: matrix4x4, n: matrix4x4): matrix4x4;
-export function add(m: any, n: any): any {
-  switch (m.length) {
-    case 2:
-      return [
-        m[0] + n[0],
-        m[1] + n[1]
-      ];
-    case 3:
-      return [
-        m[0] + n[0],
-        m[1] + n[1],
-        m[2] + n[2]
-      ];
-    case 4:
-      return [
-        m[0] + n[0],
-        m[1] + n[1],
-        m[2] + n[2],
-        m[3] + n[3]
-      ];
-    case 9:
-      return [
-        m[0] + n[0], m[1] + n[1], m[2] + n[2],
-        m[3] + n[3], m[4] + n[4], m[5] + n[5],
-        m[6] + n[6], m[7] + n[7], m[8] + n[8]
-      ];
-    case 16:
-      return [
-        m[0] + n[0], m[1] + n[1], m[2] + n[2], m[3] + n[3],
-        m[4] + n[4], m[5] + n[5], m[6] + n[6], m[7] + n[7],
-        m[8] + n[8], m[9] + n[9], m[10] + n[10], m[11] + n[11],
-        m[12] + n[12], m[13] + n[13], m[14] + n[14], m[15] + n[15]
-      ];
+export function transpose(m: matrix2x2): matrix2x2;
+export function transpose(m: matrix2x3): matrix3x2;
+export function transpose(m: matrix2x4): matrix4x2;
+export function transpose(m: matrix3x2): matrix2x3;
+export function transpose(m: matrix3x3): matrix3x3;
+export function transpose(m: matrix3x4): matrix4x3;
+export function transpose(m: matrix4x2): matrix2x4;
+export function transpose(m: matrix4x3): matrix3x4;
+export function transpose(m: matrix4x4): matrix4x4;
+export function transpose(m: matrix): matrix {
+  // TODO
+  return;
+}
+export function add<F extends floats>(f0: F, f1: F): F {
+  switch (f0.length) {
+    case 2: return [
+      f0[0] + f1[0],
+      f0[1] + f1[1]
+    ] as F;
+    case 3: return [
+      f0[0] + f1[0],
+      f0[1] + f1[1],
+      f0[2] + f1[2],
+    ] as F;
+    case 4: return [
+      f0[0] + f1[0],
+      f0[1] + f1[1],
+      f0[2] + f1[2],
+      f0[3] + f1[3],
+    ] as F;
   }
 }
-export function sub(m: float2, n: float2): float2;
-export function sub(m: float3, n: float3): float3;
-export function sub(m: float4, n: float4): float4;
-export function sub(m: matrix2x2, n: matrix2x2): matrix2x2;
-export function sub(m: matrix3x3, n: matrix3x3): matrix3x3;
-export function sub(m: matrix4x4, n: matrix4x4): matrix4x4;
-export function sub(m: any, n: any): any {
-  switch (m.length) {
-    case 2:
-      return [
-        m[0] - n[0],
-        m[1] - n[1]
-      ];
-    case 3:
-      return [
-        m[0] - n[0],
-        m[1] - n[1],
-        m[2] - n[2]
-      ];
-    case 4:
-      return [
-        m[0] - n[0],
-        m[1] - n[1],
-        m[2] - n[2],
-        m[3] - n[3]
-      ];
-    case 9:
-      return [
-        m[0] - n[0], m[1] - n[1], m[2] - n[2],
-        m[3] - n[3], m[4] - n[4], m[5] - n[5],
-        m[6] - n[6], m[7] - n[7], m[8] - n[8]
-      ];
-    case 16:
-      return [
-        m[0] - n[0], m[1] - n[1], m[2] - n[2], m[3] - n[3],
-        m[4] - n[4], m[5] - n[5], m[6] - n[6], m[7] - n[7],
-        m[8] - n[8], m[9] - n[9], m[10] - n[10], m[11] - n[11],
-        m[12] - n[12], m[13] - n[13], m[14] - n[14], m[15] - n[15]
-      ];
+export function addm<M extends matrix>(m0: M, m1: M): M {
+  return m0.map(<F extends floats>(e: F, i: number): F => add<F>(e, m1[i] as F)) as M;
+}
+export function sub<F extends floats>(f0: F, f1: F): F {
+  switch (f0.length) {
+    case 2: return [
+      f0[0] - f1[0],
+      f0[1] - f1[1]
+    ] as F;
+    case 3: return [
+      f0[0] - f1[0],
+      f0[1] - f1[1],
+      f0[2] - f1[2],
+    ] as F;
+    case 4: return [
+      f0[0] - f1[0],
+      f0[1] - f1[1],
+      f0[2] - f1[2],
+      f0[3] - f1[3],
+    ] as F;
   }
 }
-export const mul = (s: float, m: float2 | float3 | float4 | matrix2x2 | matrix3x3 | matrix4x4) => m.map(v => v * s);
+export function subm<M extends matrix>(m0: M, m1: M): M {
+  return m0.map(<F extends floats>(e: F, i: number): F => sub<F>(e, m1[i] as F)) as M;
+}
 
-export function dot(m: float2, n: float2): float;
-export function dot(m: float3, n: float3): float;
-export function dot(m: float4, n: float4): float;
-export function dot(m: any, n: any): float {
+export const mul = <F extends floats>(s: float, f: F): F => f.map(v => v * s) as F;
+export const mulm = <M extends matrix>(s: float, m: M): M => m.map(f => mul(s, f)) as M;
+
+export function dot<F extends floats>(m: F, n: F): float {
   switch (m.length) {
     case 2: return m[0] * n[0] + m[1] * n[1];
     case 3: return m[0] * n[0] + m[1] * n[1] + m[2] * n[2];
@@ -154,68 +173,51 @@ export function dot(m: any, n: any): float {
   }
 }
 export function dotm(m: matrix2x2, n: matrix2x2): matrix2x2;
+export function dotm(m: matrix2x3, n: matrix3x2): matrix3x2;
+export function dotm(m: matrix2x4, n: matrix4x2): matrix4x2;
+export function dotm(m: matrix2x2, n: matrix2x3): matrix2x3;
+export function dotm(m: matrix2x3, n: matrix3x3): matrix3x3;
+export function dotm(m: matrix2x4, n: matrix4x3): matrix4x3;
+export function dotm(m: matrix2x2, n: matrix2x4): matrix2x4;
+export function dotm(m: matrix2x3, n: matrix3x4): matrix3x4;
+export function dotm(m: matrix2x4, n: matrix4x4): matrix4x4;
+
+export function dotm(m: matrix3x2, n: matrix2x2): matrix2x2;
+export function dotm(m: matrix3x3, n: matrix3x2): matrix3x2;
+export function dotm(m: matrix3x4, n: matrix4x2): matrix4x2;
+export function dotm(m: matrix3x2, n: matrix2x3): matrix2x3;
 export function dotm(m: matrix3x3, n: matrix3x3): matrix3x3;
+export function dotm(m: matrix3x4, n: matrix4x3): matrix4x3;
+export function dotm(m: matrix3x2, n: matrix2x4): matrix2x4;
+export function dotm(m: matrix3x3, n: matrix3x4): matrix3x4;
+export function dotm(m: matrix3x4, n: matrix4x4): matrix4x4;
+
+export function dotm(m: matrix4x2, n: matrix2x2): matrix2x2;
+export function dotm(m: matrix4x3, n: matrix3x2): matrix3x2;
+export function dotm(m: matrix4x4, n: matrix4x2): matrix4x2;
+export function dotm(m: matrix4x2, n: matrix2x3): matrix2x3;
+export function dotm(m: matrix4x3, n: matrix3x3): matrix3x3;
+export function dotm(m: matrix4x4, n: matrix4x3): matrix4x3;
+export function dotm(m: matrix4x2, n: matrix2x4): matrix2x4;
+export function dotm(m: matrix4x3, n: matrix3x4): matrix3x4;
 export function dotm(m: matrix4x4, n: matrix4x4): matrix4x4;
-export function dotm(m: any, n: any): any {
-  switch (m.length) {
-    case 4: return [
-      m[0] * n[0] + m[1] * n[2],
-      m[0] * n[1] + m[1] * n[3],
-      m[2] * n[0] + m[3] * n[2],
-      m[2] * n[1] + m[3] * n[3]
-    ];
-    case 9: return [
-      m[0] * n[0] + m[1] * n[3] + m[2] * n[6],
-      m[0] * n[1] + m[1] * n[4] + m[2] * n[7],
-      m[0] * n[2] + m[1] * n[5] + m[2] * n[8],
-      // ---
-      m[3] * n[0] + m[4] * n[3] + m[5] * n[6],
-      m[3] * n[1] + m[4] * n[4] + m[5] * n[7],
-      m[3] * n[2] + m[4] * n[5] + m[5] * n[8],
-      // ---
-      m[6] * n[0] + m[7] * n[3] + m[8] * n[6],
-      m[6] * n[1] + m[7] * n[4] + m[8] * n[7],
-      m[6] * n[2] + m[7] * n[5] + m[8] * n[8]
-    ];
-    case 16: return [
-      m[0] * n[0] + m[1] * n[4] + m[2] * n[8] + m[3] * n[12],
-      m[0] * n[1] + m[1] * n[5] + m[2] * n[9] + m[3] * n[13],
-      m[0] * n[2] + m[1] * n[6] + m[2] * n[10] + m[3] * n[14],
-      m[0] * n[3] + m[1] * n[7] + m[2] * n[11] + m[3] * n[15],
-      // ---
-      m[4] * n[0] + m[5] * n[4] + m[6] * n[8] + m[7] * n[12],
-      m[4] * n[1] + m[5] * n[5] + m[6] * n[9] + m[7] * n[13],
-      m[4] * n[2] + m[5] * n[6] + m[6] * n[10] + m[7] * n[14],
-      m[4] * n[3] + m[5] * n[7] + m[6] * n[11] + m[7] * n[15],
-      // ---
-      m[8] * n[0] + m[9] * n[4] + m[10] * n[8] + m[11] * n[12],
-      m[8] * n[1] + m[9] * n[5] + m[10] * n[9] + m[11] * n[13],
-      m[8] * n[2] + m[9] * n[6] + m[10] * n[10] + m[11] * n[14],
-      m[8] * n[3] + m[9] * n[7] + m[10] * n[11] + m[11] * n[15],
-      // ---
-      m[12] * n[0] + m[13] * n[4] + m[14] * n[8] + m[15] * n[12],
-      m[12] * n[1] + m[13] * n[5] + m[14] * n[9] + m[15] * n[13],
-      m[12] * n[2] + m[13] * n[6] + m[14] * n[10] + m[15] * n[14],
-      m[12] * n[3] + m[13] * n[7] + m[14] * n[11] + m[15] * n[15]
-    ];
-  }
+export function dotm<M extends matrix, N extends matrix>(m: M, n: N): N {
+  // TODO
+  return;
 }
-export function det(m: matrix2x2): float;
-export function det(m: matrix3x3): float;
-export function det(m: matrix4x4): float;
-export function det(m: any): float {
+export function det(m: square): float {
   switch (m.length) {
-    case 4: return m[0] * m[3] - m[1] * m[2];
-    case 9: return (
-      m[0] * det([m[4], m[5], m[7], m[8]])
-    - m[1] * det([m[3], m[5], m[6], m[8]])
-    + m[2] * det([m[3], m[4], m[6], m[7]])
+    case 2: return m[0][0] * m[1][1] - m[1][0] * m[0][1];
+    case 3: return (
+      m[0][0] * det([[m[1][1], m[1][2]], [m[2][1], m[2][2]]])
+    - m[1][0] * det([[m[0][1], m[0][2]], [m[2][1], m[2][2]]])
+    + m[2][0] * det([[m[0][1], m[0][2]], [m[1][1], m[1][2]]])
     );
-    case 16: return (
-      m[0] * det([m[5], m[6], m[7], m[9], m[10], m[11], m[13], m[14], m[15]])
-    - m[1] * det([m[4], m[6], m[7], m[8], m[10], m[11], m[12], m[14], m[15]])
-    + m[2] * det([m[4], m[5], m[7], m[8], m[9],  m[11], m[12], m[13], m[15]])
-    - m[3] * det([m[4], m[5], m[6], m[8], m[9],  m[10], m[12], m[13], m[14]])
+    case 4: return (
+      m[0][0] * det([[m[1][1], m[1][2], m[1][3]], [m[2][1], m[2][2], m[2][3]], [m[3][1], m[3][2], m[3][3]]])
+    - m[1][0] * det([[m[0][1], m[0][2], m[0][3]], [m[2][1], m[2][2], m[2][3]], [m[3][1], m[3][2], m[3][3]]])
+    + m[2][0] * det([[m[0][1], m[0][2], m[0][3]], [m[1][1], m[1][2], m[1][3]], [m[3][1], m[3][2], m[3][3]]])
+    - m[3][0] * det([[m[0][1], m[0][2], m[0][3]], [m[1][1], m[1][2], m[1][3]], [m[2][1], m[2][2], m[2][3]]])
     );
   }
 }
@@ -225,9 +227,9 @@ export function cross(m: any, n: any): any {
   switch (m.length) {
     case 2: return det(matrix2x2(m, n));
     case 3: return [
-      det([m[1], n[1], m[2], n[2]]),
-      det([m[0], n[0], m[2], n[2]]),
-      det([m[0], n[0], m[1], n[1]]),
+      det([[m[1], n[2]], [m[1], n[2]]]),
+      det([[m[0], n[2]], [m[0], n[2]]]),
+      det([[m[0], n[1]], [m[0], n[1]]]),
     ];
   }
 }
